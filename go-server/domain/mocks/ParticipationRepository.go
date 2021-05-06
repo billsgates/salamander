969f96ca91cdca4e56542643aa9eec454c5d458a
kevinyu0506 <kevinyu05062006@gmail.com>
2021-05-06 14:15:11 +0800

    Regenerate mockery object

--- go-server/domain/mocks/ParticipationRepository.go
+++ go-server/domain/mocks/ParticipationRepository.go
@@ -71,3 +71,17 @@ func (_m *ParticipationRepository) IsAdmin(ctx context.Context, roomId int32, us
 
 	return r0, r1
 }
+
+// LeaveRoom provides a mock function with given fields: ctx, roomId, userId
+func (_m *ParticipationRepository) LeaveRoom(ctx context.Context, roomId int32, userId int32) error {
+	ret := _m.Called(ctx, roomId, userId)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, int32, int32) error); ok {
+		r0 = rf(ctx, roomId, userId)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
--- go-server/domain/mocks/RoomUsecase.go
+++ go-server/domain/mocks/RoomUsecase.go
@@ -85,3 +85,17 @@ func (_m *RoomUsecase) JoinRoom(ctx context.Context, code string) error {
 
 	return r0
 }
+
+// LeaveRoom provides a mock function with given fields: ctx, roomId, userId
+func (_m *RoomUsecase) LeaveRoom(ctx context.Context, roomId int32, userId int32) error {
+	ret := _m.Called(ctx, roomId, userId)
+
+	var r0 error
+	if rf, ok := ret.Get(0).(func(context.Context, int32, int32) error); ok {
+		r0 = rf(ctx, roomId, userId)
+	} else {
+		r0 = ret.Error(0)
+	}
+
+	return r0
+}
