2cbecdd03f091b8a2b2c0a841e1acd742fb7fd1c
kevinyu0506 <kevinyu05062006@gmail.com>
2021-05-12 14:49:20 +0800

    Update swagger.yaml & README.md

--- go-server/go/api_rooms.go
+++ go-server/go/api_rooms.go
@@ -17,6 +17,11 @@ func CreateRoom(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
 }
 
+func DeleteRoom(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+	w.WriteHeader(http.StatusOK)
+}
+
 func GetMenuInfo(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
