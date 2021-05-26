19c925e421e81f932c286f1f84944ede9e32ae1d
Frank Chen <biohazard93011@ntu.im>
2021-05-26 19:11:33 +0800

    [api] add get & patch user/:userId/rating endpoint

--- go-server/user/usecase/user_ucase.go
+++ go-server/user/usecase/user_ucase.go
@@ -63,11 +63,45 @@ func (u *userUsecase) GetByEmailPassword(c context.Context, email string, passwo
 	return res, nil
 }
 
+func (u *userUsecase) GetUserRating(c context.Context, id string) (res *domain.RatingResponse, err error) {
+	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
+	defer cancel()
+
+	userRes, err := u.userRepo.GetByID(ctx, id)
+	if err != nil {
+		return nil, err
+	}
+	res = &domain.RatingResponse{
+		Rating:      userRes.Rating,
+		RatingCount: userRes.RatingCount,
+	}
+	return res, nil
+}
+
 func (u *userUsecase) Update(c context.Context, user *domain.UserRequest) (err error) {
 	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
 	defer cancel()
 
 	err = u.userRepo.Update(ctx, user)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (u *userUsecase) UpdateRating(c context.Context, id string, rating int32) (err error) {
+	ctx, cancel := context.WithTimeout(c, u.contextTimeout)
+	defer cancel()
+
+	// get original rating to calculate new avg rating
+	userRes, err := u.userRepo.GetByID(ctx, id)
+	if err != nil {
+		return err
+	}
+
+	newRating := (userRes.Rating*float32(userRes.RatingCount) + float32(rating)) / float32(userRes.RatingCount+1)
+
+	err = u.userRepo.UpdateRating(ctx, id, newRating)
 	if err != nil {
 		return err
 	}
