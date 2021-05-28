d427cb263d5c69a018bf9f851905739812666e24
kevinyu0506 <kevinyu05062006@gmail.com>
2021-05-28 02:40:18 +0000

    Update init-letsencrypt.sh

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(billsgate.club www.billsgate.club)
+domains=(billsgate.club)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="kevinyu05062006@gmail.com" # Adding a valid address is strongly recommended
